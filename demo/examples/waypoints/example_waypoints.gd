c095ee9c5d1f7f039d43a7edea693faed6b109d8
Serhii Snitsaruk <limbonaut@pm.me>
2023-07-23 18:16:52 +0200

    Update demo project

--- demo/examples/waypoints/example_waypoints.gd
+++ demo/examples/waypoints/example_waypoints.gd
@@ -1,5 +1,5 @@
 #*
-#* test_waypoints.gd
+#* example_waypoints.gd
 #* =============================================================================
 #* Copyright 2021-2023 Serhii Snitsaruk
 #*
@@ -11,18 +11,18 @@
 
 extends Node2D
 
-@onready var agent: CharacterBody2D = $Agent
-@onready var agent_2: CharacterBody2D = $Agent2
+@onready var agent1: CharacterBody2D = $Agent
+@onready var agent2: CharacterBody2D = $Agent2
 
 
 func _ready() -> void:
 	var waypoints: Array[Node] = $Waypoints.get_children()
 
 	for wp in waypoints:
-		agent.add_waypoint(wp.global_position)
+		agent1.add_waypoint(wp.global_position)
 
 	waypoints.reverse()
 	for wp in waypoints:
-		agent_2.add_waypoint(wp.global_position)
+		agent2.add_waypoint(wp.global_position)
 
 
