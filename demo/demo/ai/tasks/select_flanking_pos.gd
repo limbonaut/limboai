d0ff58052d59670a3cf168591a886779e4ca8fcf
Serhii Snitsaruk <limbonaut@pm.me>
2024-02-01 12:36:05 +0100

    Demo: Add AI for ranged enemy

--- demo/demo/ai/tasks/select_flanking_pos.gd
+++ demo/demo/ai/tasks/select_flanking_pos.gd
@@ -1,5 +1,5 @@
 #*
-#* GetFlankPos.gd
+#* select_flanking_pos.gd
 #* =============================================================================
 #* Copyright 2021-2024 Serhii Snitsaruk
 #*
@@ -10,25 +10,43 @@
 #*
 @tool
 extends BTAction
-## GetFlankPos
+## SelectFlankingPos on the side of a target, and return SUCCESS.
+## Returns FAILURE, if the target is not valid.
 
+## Blackboard variable that holds current target (should be a Node2D instance).
 @export var target_var: String = "_target"
+
+## Should closest side be selected?
+@export var closest_side: bool = false
+
+## Minimum range relative to the target.
+@export var range_min: int = 300
+
+## Maximum range relative to the target.
+@export var range_max: int = 400
+
+## Blackboard variable that will be used to store selected position.
 @export var position_var: String = "flank_pos"
-@export var distance: float = 300.0
 
 # Display a customized name (requires @tool).
 func _generate_name() -> String:
-	return "GetFlankPos %s -> %s" % [
+	return "SelectFlankingPos  target: %s  range: [%s, %s]  closest: %s  ➜ %s" % [
 		LimboUtility.decorate_var(target_var),
+		range_min,
+		range_max,
+		closest_side,
 		LimboUtility.decorate_var(position_var)]
 
 # Called each time this task is ticked (aka executed).
 func _tick(_delta: float) -> Status:
-	var target: CharacterBody2D = blackboard.get_var(target_var)
+	var target := blackboard.get_var(target_var) as Node2D
 	if not is_instance_valid(target):
 		return FAILURE
+
 	var dir: float = signf(target.global_position.x - agent.global_position.x)
+	if closest_side:
+		dir *= -1.0
 	var flank_pos: Vector2 = target.global_position
-	flank_pos.x += dir * distance
+	flank_pos.x += dir * randf_range(range_min, range_max)
 	blackboard.set_var(position_var, flank_pos)
 	return SUCCESS
