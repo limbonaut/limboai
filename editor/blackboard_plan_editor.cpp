022d95434d9c98856e2ae34c641c797b4b37ea09
Wilson E. Alvarez <wilson.e.alvarez@rubonnek.com>
2024-03-02 11:09:11 -0500

    Add missing MarginContainer header
    Due to upstream change:
    
    	https://github.com/godotengine/godot/commit/7884d632812996ef26184412846c533276cb4cc0

--- editor/blackboard_plan_editor.cpp
+++ editor/blackboard_plan_editor.cpp
@@ -21,6 +21,7 @@
 #include "editor/editor_interface.h"
 #include "editor/themes/editor_scale.h"
 #include "scene/gui/line_edit.h"
+#include "scene/gui/margin_container.h"
 #include "scene/gui/panel_container.h"
 #include "scene/resources/style_box_flat.h"
 #endif // LIMBOAI_MODULE
