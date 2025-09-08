81b17b4bc887a8933adfb97106aa2a53872ca03e
Wilson E. Alvarez <wilson.e.alvarez@rubonnek.com>
2025-09-08 17:30:42 -0400

    Do not include editor headers in template builds when compiling as module

--- compat/editor_settings.h
+++ compat/editor_settings.h
@@ -14,7 +14,10 @@
 
 #ifdef LIMBOAI_MODULE
 
+#ifdef TOOLS_ENABLED
+#include "editor/editor_interface.h"
 #include "editor/settings/editor_settings.h"
+#endif // TOOLS_ENABLED
 
 #define EDITOR_SETTINGS() (EditorSettings::get_singleton())
 
