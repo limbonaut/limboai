149643b1374533be152e5e624f48245da71a3ff9
Serhii Snitsaruk <limbonaut@pm.me>
2026-02-22 14:36:02 +0100

    Editor: Fix double-click on task switching to Script tab when external editor is set

--- compat/editor.cpp
+++ compat/editor.cpp
@@ -12,43 +12,25 @@
 
 #ifdef TOOLS_ENABLED
 
-#ifdef LIMBOAI_MODULE
-#include "editor/editor_main_screen.h"
-#include "editor/editor_node.h"
-#include "editor/script/script_editor_plugin.h"
-#endif // LIMBOAI_MODULE
-
-#ifdef LIMBOAI_GDEXTENSION
 #include "resource_loader.h"
-#include <godot_cpp/classes/script.hpp>
+
+#ifdef LIMBOAI_MODULE
+#include "editor/editor_interface.h"
+#include "editor/script/script_editor_plugin.h"
+#elif LIMBOAI_GDEXTENSION
+#include <godot_cpp/classes/editor_interface.hpp>
 #include <godot_cpp/classes/script_editor.hpp>
-#include <godot_cpp/classes/script_editor_base.hpp>
-#endif // LIMBOAI_GDEXTENSION
+#endif
 
 void SHOW_BUILTIN_DOC(const String &p_topic) {
-#ifdef LIMBOAI_MODULE
-	ScriptEditor::get_singleton()->goto_help(p_topic);
-	EditorNode::get_singleton()->get_editor_main_screen()->select(EditorMainScreen::EDITOR_SCRIPT);
-#elif LIMBOAI_GDEXTENSION
-	TypedArray<ScriptEditorBase> open_editors = EditorInterface::get_singleton()->get_script_editor()->get_open_script_editors();
-	ERR_FAIL_COND_MSG(open_editors.size() == 0, "Can't open help page. Need at least one script open in the script editor.");
-	ScriptEditorBase *seb = Object::cast_to<ScriptEditorBase>(open_editors.front());
-	ERR_FAIL_NULL(seb);
-	seb->emit_signal("go_to_help", p_topic);
-#endif
+	EditorInterface::get_singleton()->get_script_editor()->goto_help(p_topic);
+	EditorInterface::get_singleton()->set_main_screen_editor("Script");
 }
 
 void EDIT_SCRIPT(const String &p_path) {
-#ifdef LIMBOAI_MODULE
-	Ref<Resource> res = ScriptEditor::get_singleton()->open_file(p_path);
+	Ref<Resource> res = RESOURCE_LOAD(p_path, "Script");
 	ERR_FAIL_COND_MSG(res.is_null(), "Failed to load script: " + p_path);
-	EditorNode::get_singleton()->edit_resource(res);
-#elif LIMBOAI_GDEXTENSION
-	Ref<Script> res = RESOURCE_LOAD(p_path, "Script");
-	ERR_FAIL_COND_MSG(res.is_null(), "Failed to load script: " + p_path);
-	EditorInterface::get_singleton()->edit_script(res);
-	EditorInterface::get_singleton()->set_main_screen_editor("Script");
-#endif
+	EditorInterface::get_singleton()->edit_resource(res);
 }
 
 #endif // ! TOOLS_ENABLED
