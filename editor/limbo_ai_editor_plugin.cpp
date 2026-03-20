c7b7138e60892d2fd83c2c331a2d365bb6a2ef89
Wilson E. Alvarez <wilson.e.alvarez@rubonnek.com>
2026-03-20 16:02:39 -0400

    Improve Ref ownership safety
    Due to upstream change:
    
    		https://github.com/godotengine/godot/commit/05c33acbb1e7189719d1edd6105cbb23065bdd9d

--- editor/limbo_ai_editor_plugin.cpp
+++ editor/limbo_ai_editor_plugin.cpp
@@ -1952,14 +1952,14 @@ void LimboAIEditorPlugin::_notification(int p_notification) {
 			add_debugger_plugin(memnew(LimboDebuggerPlugin));
 			add_inspector_plugin(memnew(EditorInspectorPluginBBPlan));
 
-			EditorInspectorPluginVariableName *var_plugin = memnew(EditorInspectorPluginVariableName);
+			Ref<EditorInspectorPluginVariableName> var_plugin = memnew(EditorInspectorPluginVariableName);
 			var_plugin->set_editor_plan_provider(Callable(limbo_ai_editor, "get_edited_blackboard_plan"));
 			add_inspector_plugin(var_plugin);
 
-			EditorInspectorPluginPropertyPath *path_plugin = memnew(EditorInspectorPluginPropertyPath);
+			Ref<EditorInspectorPluginPropertyPath> path_plugin = memnew(EditorInspectorPluginPropertyPath);
 			add_inspector_plugin(path_plugin);
 
-			EditorInspectorPluginBBParam *param_plugin = memnew(EditorInspectorPluginBBParam);
+			Ref<EditorInspectorPluginBBParam> param_plugin = memnew(EditorInspectorPluginBBParam);
 			param_plugin->set_plan_getter(Callable(limbo_ai_editor, "get_edited_blackboard_plan"));
 			add_inspector_plugin(param_plugin);
 		} break;
